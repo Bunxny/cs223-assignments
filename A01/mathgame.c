64d99f78c7c4d1581c560ef5d00ca35d5f1096bf
Anna Nguyen <anguyen2@goldengate.cs.brynmawr.edu>
2023-02-01 14:36:01 -0500

    Descriptive message

--- A01/mathgame.c
+++ A01/mathgame.c
@@ -1,6 +1,6 @@
 /***************************************************
  * mathgame.c
- * Author: 
+ * Author:Anna Nguyen
  * Implements a math game
  */
 
@@ -16,7 +16,7 @@ int main() {
 	int input;
 	int count = 0;
 	printf("Welcome to Math Game!\nHow many rounds do you want to play?\n");
-	scanf("%d", &rounds);
+	scanf(" %d", &rounds);
 	int ret[3];
 	srand(time(0));
 	for ( int i = 0; i < rounds; i++) {
@@ -24,13 +24,13 @@ int main() {
 		secondNum = rand()% 9 + 1;
 		answer = firstNum + secondNum;
 		printf("%d + %d = ? \n", firstNum, secondNum);
-		scanf("%d", &input);
+		scanf(" %d", &input);
 		if ( input == answer) {
 			printf("Correct :)\n");
 			count = count + 1;
 		} else {
 			printf("Incorrect\n");
-		}
+  	}
 	}
 	printf("You got %d/%d correct\n", count, rounds);
 	return 0;
