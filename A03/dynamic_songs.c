8d700fa12f14dd733a569084cc10ed1442381725
Anna Nguyen <anguyen2@goldengate.cs.brynmawr.edu>
2023-02-08 23:53:29 -0500

    assignment 2 complete

--- A03/dynamic_songs.c
+++ A03/dynamic_songs.c
@@ -1,10 +1,55 @@
 /*----------------------------------------------
- * Author: 
- * Date: 
- * Description
+ * Author:Anna Nguyen
+ * Date:February 5, 2023
+ * Description:Reads songs from a text file into an array
+ * and prints it out
  ---------------------------------------------*/
+
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int main() {
+  FILE *infile;
+  infile = fopen("songlist.csv", "r");
+  if (infile == NULL) {
+    printf("Error: unable to open file %s\n", "songlist.csv");
+    exit(1);
+  }
+  char *token;
+  char *delim = ",";
+  char *line;
+  char **data;
+  line = malloc(128 * sizeof(char));
+  if (line == NULL) {
+    printf("Error: malloc failed\n");
+    exit(1);
+  }
+  line = fgets(line, 128, infile);
+  token = strtok(line, delim);
+  int len = atoi(token);
+  data = malloc(sizeof(char *) * len);
+  for (int i = 0; i < len; i++) {
+    data[i] = malloc(sizeof(char) * 7);
+  }
+  line = fgets(line, 128, infile);
+  for (int i = 0; i < len; i++) {
+    line = fgets(line, 128, infile);
+    token = strtok(line, delim);
+    int count=0;
+    while (token != NULL) {
+      data[i][count] = *token;
+      printf("%s\n", data[count]);
+      count++;
+      token = strtok(NULL, delim);
+    }
+  }
+  for (int j = 0; j < len; j++) {
+    for (int k = 0; k < 7; k++) {
+      printf("%s\n", data[j][k]);
+    }
+  }
+  free(data);
+  free(line);
   return 0;
 }
--- A03/slow.c
+++ A03/slow.c
@@ -1,10 +1,36 @@
 /*----------------------------------------------
- * Author: 
- * Date: 
- * Description
+ * Author:Anna Nguyen
+ * Date:February 5, 2023
+ * Description: This programs puts n numbers of '.'
+ * between each letter in a phrase.
  ---------------------------------------------*/
 #include <stdio.h>
-
+#include <stdlib.h>
+#include <string.h>
 int main() {
+  char buff[32];
+  int pause;
+  printf("Pause length: ");
+  scanf(" %d", &pause);
+  printf("\nText: ");
+  scanf(" %s", buff);
+  int len = strlen(buff);
+  char *text;
+  text = malloc((sizeof(char) * (pause * len)) + len);
+  int lenT = (sizeof(char) * (pause * len)) + len;
+  //(('.' * pauseLength) * numberOfCharacters) + numberOfCharacters + 1
+  for (int i = 0; i < len; i++) {
+    text[i + (i * pause)] = buff[i];
+    for (int j = 1; j <= pause; j++) {
+      text[i + (i * pause) + j] = '.';
+    }
+  }
+  printf("\nEnt-phrase is ");
+  for (int k = 0; k < lenT; k++) {
+    printf("%c", text[k]);
+  }
+  printf("\n");
+  free(text);
   return 0;
 }
+
