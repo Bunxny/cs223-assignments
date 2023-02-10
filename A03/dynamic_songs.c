3d92c7bd96b238db90f85f0cb070f8254212a3ee
Anna Nguyen <anguyen2@goldengate.cs.brynmawr.edu>
2023-02-10 17:00:05 -0500

    assignment 3 complete

--- A03/dynamic_songs.c
+++ A03/dynamic_songs.c
@@ -1,4 +1,4 @@
-/*----------------------------------------------
+ /*----------------------------------------------
  * Author:Anna Nguyen
  * Date:February 5, 2023
  * Description:Reads songs from a text file into an array
@@ -9,6 +9,18 @@
 #include <stdlib.h>
 #include <string.h>
 
+struct song {
+  int ms;
+  int min;
+  int sec;
+  float energy;
+  float tempo;
+  float valence;
+  float danceability;
+  char artist[128];
+  char title[128];
+};
+
 int main() {
   FILE *infile;
   infile = fopen("songlist.csv", "r");
@@ -18,38 +30,55 @@ int main() {
   }
   char *token;
   char *delim = ",";
-  char *line;
-  char **data;
-  line = malloc(128 * sizeof(char));
-  if (line == NULL) {
-    printf("Error: malloc failed\n");
-    exit(1);
-  }
-  line = fgets(line, 128, infile);
+  char line[2046];
+  strcpy(line, fgets(line, 128, infile));
   token = strtok(line, delim);
   int len = atoi(token);
-  data = malloc(sizeof(char *) * len);
+  struct song* data = malloc(sizeof(struct song) * len);
+  strcpy(line, fgets(line, 128, infile));
   for (int i = 0; i < len; i++) {
-    data[i] = malloc(sizeof(char) * 7);
-  }
-  line = fgets(line, 128, infile);
-  for (int i = 0; i < len; i++) {
-    line = fgets(line, 128, infile);
+    strcpy(line, fgets(line, 128, infile));
     token = strtok(line, delim);
-    int count=0;
+    int count = 0;
+    struct song songs;
     while (token != NULL) {
-      data[i][count] = *token;
-      printf("%s\n", data[count]);
+      if (count == 0) {
+        strcpy(songs.title, token);
+      } else if (count == 1) {
+        strcpy(songs.artist, token);
+      } else if (count == 2) {
+        songs.ms = atoi(token);
+        songs.sec = (songs.ms/1000) % 60;
+        songs.min = (songs.ms/1000) / 60;
+      } else if (count == 3) {
+        songs.danceability = atof(token);
+      } else if (count == 4) {
+        songs.energy = atof(token);
+      } else if (count == 5) {
+        songs.tempo = atof(token);
+      } else {
+        songs.valence = atof(token);
+      }
       count++;
       token = strtok(NULL, delim);
     }
+    data[i] = songs;
   }
-  for (int j = 0; j < len; j++) {
-    for (int k = 0; k < 7; k++) {
-      printf("%s\n", data[j][k]);
+  for (int i = 0; i < len; i++) {
+    if (data[i].sec <= 9) {
+      printf("\n%d) %-20s artist: %-20s duration: %d:0%-20d danceability: %.2f\n", i,
+        data[i].title, data[i].artist, data[i].min, data[i].sec,
+          data[i].danceability);
+    } else {
+      printf("\n%d) %-20s artist: %-20s duration: %d:0%-20d danceability: %.2f\n", i,
+        data[i].title, data[i].artist, data[i].min, data[i].sec,
+          data[i].danceability);
     }
+    printf("energy: %-20.3f tempo: %-20.3f valence: %-20.3f",
+      data[i].energy, data[i].tempo, data[i].valence);
+    printf("\n=======================\n");
   }
+  fclose(infile);
   free(data);
-  free(line);
   return 0;
 }
